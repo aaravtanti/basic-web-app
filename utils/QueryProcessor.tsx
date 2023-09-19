afd2b7b7809652c0a9bb3cec342eee13687b8e1a
Aarav Tanti <aaravtanti@aaravs-mbp-2.wifi.local.cmu.edu>
2023-09-19 16:25:43 -0400

    max tested

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -16,17 +16,28 @@ export default function QueryProcessor(query: string): string {
       "aarav"
     );
   }
+
   const addMatch = query.match(/What is (\d+) plus (\d+)/);
   if (addMatch) {
     const x: number = parseInt(addMatch[1]);
     const y: number = parseInt(addMatch[2]);
     return (x+y).toString();
   }
+
   const multMatch = query.match(/What is (\d+) multiplied by (\d+)/);
   if (multMatch) {
     const x: number = parseInt(multMatch[1]);
     const y: number = parseInt(multMatch[2]);
     return (x*y).toString();
+  }
+
+  const largest = query.match(/Which of the following numbers is the largest: (\d+)(\d+)(\d+)/);
+  if (largest) {
+    const x: number = parseInt(largest[1]);
+    const y: number = parseInt(largest[2]);
+    const z: number = parseInt(largest[3]);
+    const a = Math.max(x, y);
+    return Math.max(a, z).toString();
   }
 
   return "";
